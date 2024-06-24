8289cedcbce067a0387b857ee9b0f56fb1586dd2
Ewelina Skibinska <e.skibinska@wellcome.org>
2024-06-24 15:13:23 +0100

    Add github action, update package, update tokens

--- build.js
+++ build.js
@@ -1,32 +1,22 @@
-import fs from 'node:fs';
-import { registerTransforms } from '@tokens-studio/sd-transforms';
 import StyleDictionary from 'style-dictionary';
-import tokens from './tokens.json' assert { type: "json" };
-import config from './config.js';
+import { registerTransforms } from '@tokens-studio/sd-transforms';
 
-function slugify(str) {
-  return str
-    .replace(/^\s+|\s+$/g, '')
-    .toLowerCase()
-    .replace(/[^a-z0-9 -]/g, '')
-    .replace(/\s+/g, '-')
-    .replace(/-+/g, '-');
-}
+registerTransforms(StyleDictionary);
 
-// Split the tokens.json (from Tokens Studio) into its top-level keys -- useful
-// if we want to e.g. prefix variables by their type
-Object.entries(tokens).map(([key, value]) => {
-  if (key.startsWith('$')) return; // Tokens Studio meta info, not required for variables
+const sd = new StyleDictionary('config.json');
 
-  try {
-    fs.writeFileSync(`./src/${slugify(key)}.json`, JSON.stringify(value), { flag: 'w' })
-  } catch (err) {
-    console.log(err);
+// Define the array of excluded token paths
+const excludedPaths = [
+  'tokenSetOrder',
+  'asset',
+  'annotations'
+];
+
+sd.registerFilter({
+  name: 'custom/excludeTokens',
+  filter: (prop) => {
+    return !prop.path.some(part => excludedPaths.includes(part));
   }
 });
 
-registerTransforms(StyleDictionary);
-const sd = new StyleDictionary(config);
-
-sd.cleanAllPlatforms();
 sd.buildAllPlatforms();
