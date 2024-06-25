d07bd5cb78f7556aba599fc33f716ed1932af481
Chris Liddell <chrisliddell1991@gmail.com>
2024-06-25 16:13:39 +0100

    task: adds Github action workflows for versioning and release

---
+++ scripts/create-changeset-file.py
@@ -0,0 +1,7 @@
+file = open(".changeset/changeset.md", "w")
+
+file.write(f"---\n")
+file.write(f'"wellcome-trust/design-tokens": patch\n')
+file.write(f"---\n")
+file.write(f"\n")
+file.write(f"list of changes\n")
