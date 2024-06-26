ea542b38aab64329e7e93685d3534bab7543a22d
Chris Liddell <chrisliddell1991@gmail.com>
2024-06-26 16:04:11 +0100

    task: adds prettier and configures with linter, enables recommended rules & applies them

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,17 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-plugin-prettier/recommended";
 
 export default [
   {
-    languageOptions: { 
+    languageOptions: {
       globals: globals.browser,
+    },
+    plugins: {
+      prettier,
     },
   },
   pluginJs.configs.recommended,
+  prettierConfig,
 ];
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import { createRequire } from 'node:module';
+import { createRequire } from "node:module";
 
 const require = createRequire(import.meta.url);
 
-const tokensFromJson = require('./dist/json/variables.json');
+const tokensFromJson = require("./dist/json/variables.json");
 
 export const tokens = tokensFromJson;
 // export const icons = someObjectContainingTheIcons
