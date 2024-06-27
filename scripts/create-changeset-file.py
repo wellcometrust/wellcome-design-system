bfd1aa0d9d836078a94787e709257e80ebe5308f
Chris Liddell <chrisliddell1991@gmail.com>
2024-06-27 14:57:05 +0100

    task: updates changeset process to pull semver from pull request's label; ensures that version increments only occur on specific branch (token-studio)

--- scripts/create-changeset-file.py
+++ scripts/create-changeset-file.py
@@ -1,7 +1,11 @@
+import sys
+
 file = open(".changeset/changeset.md", "w")
 
 file.write(f"---\n")
-file.write(f'"wellcome-trust/design-tokens": patch\n')
+file.write(
+    f'"wellcome-trust/wellcome-design-system": {sys.argv[1].replace("semver:", "")}\n'
+)
 file.write(f"---\n")
 file.write(f"\n")
-file.write(f"list of changes\n")
+file.write(f"- changes\n")
